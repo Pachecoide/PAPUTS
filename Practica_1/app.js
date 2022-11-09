c5e68a7386f81580033ed776bb5df01bdad1d6fb
Jorge Jonathan Vera Rodriguez <verj050907hmcrdra8@soycecytem.mx>
2022-11-09 09:12:07 -0600

    PAPUTS 1.3

--- Practica_1/app.js
+++ Practica_1/app.js
@@ -10,10 +10,85 @@ const cargarPosts=async()=>{
         <th scope="row">${item.id}</th>
         <td>${item.title}</td>
         <td>${item.body}</td>
-        <td><button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#editModal"> <i class="bi bi-pencil-square"></i> Editar</button></td>
-        <td><button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="bi bi-backspace m-2"></i>Eliminar</button></td>
+        <td><button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#editModal" onclick="cargarPost(${item.id})"> <i class="bi bi-pencil-square"></i> Editar</button></td>
+        <td><button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#deleteModal" onclick="eliminarPost(${item.id})"><i class="bi bi-backspace m-2"></i>Eliminar</button></td>
         </tr>`;
     })
+}
+
+const cargarPost=async(id)=>{
+  const api= await fetch('https://jsonplaceholder.typicode.com/posts/'+id);
+    const respuesta=await api.json();
+    console.log(respuesta)
+    document.querySelector("#etitle").value=respuesta.id;
+    document.querySelector("#ebody").value=respuesta.body;
+    document.querySelector("#eid").value=respuesta.id;
+    document.querySelector("#euserid").value=respuesta.userId;
+}
+
+const guardarPost=async()=>{
+  let title=document.querySelector("#etitle").value;
+  let body=document.querySelector("#ebody").value;
+  let id=document.querySelector("#eid").value;
+  let userId=document.querySelector("#euserid").value;
+  const post={
+    title:title,
+    body:body,
+    id:id,
+    userId:userId
+}
+const api=await fetch('https://jsonplaceholder.typicode.com/posts/'+id, {
+    method: 'PUT',
+    body: JSON.stringify(post),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+const respuesta=await api.json();
+console.log(respuesta)
+if(respuesta!=null){
+  Swal.fire({
+      icon: 'success',
+      title: 'Editado',
+      text: 'Se a Editado con exito!',
+  
+    })
+}else{
+  Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Algo salio mal!',
+  
+    })
+}
+}
+
+
+const eliminarPost=(id)=>{
+  document.querySelector("#deleteid").value=id;
+}
+const deletepost=async ()=>{
+  const id=document.querySelector("#deleteid").value;
+  const api=await fetch('https://jsonplaceholder.typicode.com/posts/'+id, {
+      method: 'DELETE',
+  })
+  const respuesta=await api.json();
+  console.log(respuesta)
+  if(respuesta!=null){
+    Swal.fire({
+        icon: 'success',
+        title: 'Editado',
+        text: 'Se a Editado con exito!',
+    
+      })
+  }else{
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Algo salio mal!',
+    
+      })
+  }
 }
 
 const addpost=async ()=>{
@@ -58,4 +133,6 @@ if(respuesta!=null){
     
       })
 }
+
+
 }
